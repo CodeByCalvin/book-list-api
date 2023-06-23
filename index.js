8f22f58fd9b392878a760555627e0bf692747ee4
CodeByCalvin <calvin@calvinmerry.com>
2023-06-23 12:43:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const router = require("./router");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 try {
   const db = mongoose.connect(process.env.DB_URI, {
